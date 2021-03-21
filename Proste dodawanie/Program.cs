681f6da865c5514c85606d700e40f8ddd2d36cbd
HPieniazek <pieniazekh@gmail.com>
2021-03-22 00:33:54 +0100

    proste dodawanie(bledna odpowiedz)

---
+++ Proste dodawanie/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Proste_dodawanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long suma = 0;         
+            string wpis = Console.ReadLine();
+            var s1 = wpis.Split(" ");
+            for (int i = 0; i < s1.Length; i++)
+            {
+                long n = long.Parse(s1[i]);
+                suma += n;
+            }
+            Console.WriteLine(suma);
+            
+        }
+    }
+}
