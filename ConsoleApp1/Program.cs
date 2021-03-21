37aa92f274a03e228824189983a1aeda04375516
HPieniazek <pieniazekh@gmail.com>
2021-03-21 23:52:28 +0100

    Czy umiesz potegowac (blad wykonania)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wpis = Console.ReadLine();
+            string[] n = wpis.Split(" ");
+            double x = Convert.ToDouble(n[0]);
+            double y = Convert.ToDouble(n[1]);
+            double z = Math.Pow(x, y);
+            double a = z % 10;
+            Console.WriteLine(a);
+
+        }
+    }   
+}
