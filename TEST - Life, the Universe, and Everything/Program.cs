d82003033d62f17ceaeb656e3ac0ef19f94be99d
HPieniazek <pieniazekh@gmail.com>
2021-03-22 01:20:54 +0100

    TEST___Life__the_Universe__and_Everything

---
+++ TEST - Life, the Universe, and Everything/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TEST___Life__the_Universe__and_Everything
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1 = Console.ReadLine();
+            
+            int n = Convert.ToInt32(n1);
+            if (n != 42) 
+            {
+                Console.WriteLine(n1);
+                string n2 = Console.ReadLine();
+            }
+            if (n == 41)
+            {
+                return;
+            }   
+            
+        }
+    }
+}
---
+++ dodawanieLiczbCalkowitych/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace dodawanieLiczbCalkowitych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a1 = Console.ReadLine();
+            string b1 = Console.ReadLine();
+            string c1= Console.ReadLine();
+
+            int a = Convert.ToInt32(a1);
+            int b = Convert.ToInt32(b1);
+            int c = Convert.ToInt32(c1);
+            Console.WriteLine(a + b + c);
+        }
+    }
+}
