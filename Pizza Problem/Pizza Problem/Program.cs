53095228be18874c9079066db3f7905dc1c69526
HPieniazek <pieniazekh@gmail.com>
2021-11-01 14:22:01 +0100

    commit

---
+++ Pizza Problem/Pizza Problem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class pizza
+{
+    public static void Main()
+    {
+
+        List<long> x = new List<long>();
+        string X = Console.ReadLine();
+        long number;
+
+        if (Int64.TryParse(X, out number) && number >=1)
+        {
+            x.Add(number);
+            List<long> xList = x.Distinct().ToList();
+            List<long> list = new List<long>();
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                long value = x[i];
+                long Z = (value * value + value + 2) / 2;
+
+                list.Add(Z); 
+
+            }
+
+            foreach (int item in list)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
